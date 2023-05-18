e5d022761bf84624d78ff4d8c93b2624b2c62563
devnkheradiya <devnkheradiya@gmail.com>
2023-05-18 13:11:03 +0530

    navigation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import { Routes,Route,Link, BrowserRouter } from 'react-router-dom';
+import {HomePage} from './Components/HomePage';
+import {Apple} from './Components/Apple';
+import {NotFound} from './Components/NotFound';
+const App=() => {
+  return(
+    <BrowserRouter>
+    <div>
+      <Link to='/'
+      style={{
+        marginLeft: 5,
+      }}>
+        Home</Link>
+      <Link to='/apple'
+      style={{
+        marginLeft: 10,
+      }}>
+        Apple</Link>
+        <Link to='/applet'
+      style={{
+        marginLeft: 15,
+      }}>
+        Applet</Link>
     </div>
+    <Routes>
+      <Route path='/' element={<HomePage/>}></Route>
+      <Route path='/apple' element={<Apple/>}></Route>
+      <Route path='*' element={<NotFound/>}></Route>
+    </Routes>
+  </BrowserRouter>
   );
-}
+};
 
 export default App;
