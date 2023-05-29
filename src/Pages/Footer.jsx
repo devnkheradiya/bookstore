16c3bea5205aa0e5eab9017c8395a4dcda3729ef
devnkheradiya <devnkheradiya@gmail.com>
2023-05-29 10:30:11 +0530

    ui changes

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -18,7 +18,7 @@ const Footer = () => {
            height: 150, backgroundColor: "#F4F4F4" }}
         spacing={1}
       >
-        <img src='https://bookstore-sooty.vercel.app/static/media/site-logo.005b78aa01d0b4eadda3fa91c02202c5.svg' alt="LOGO" height={45} />
+        <img src='https://bookstore-sooty.vercel.app/static/media/site-logo.005b78aa01d0b4eadda3fa91c02202c5.svg' alt="tatva logo" height={45} />
         <Typography variant="body2" className="fs" color={"#929292"}>
            © 2015 Tatvasoft.com. All rights reserved.
         </Typography>  
